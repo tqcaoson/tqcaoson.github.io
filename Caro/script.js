c82cf2ad1b9d5ea1c12d660b541d13217463bea8
tqcaoson <tqcaoson@gmail.com>
2021-05-13 16:30:31 +0700

    update minmax

--- Caro/script.js
+++ Caro/script.js
@@ -1,271 +1,173 @@
-//I've tried to explain each JavaScript line with comments....Hope you'll understand
+let origBoard;
+let huPlayer ='O';
+let aiPlayer = 'X';
+const winCombos =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [6, 4, 2],
+  [2, 5, 8],
+  [1, 4, 7],
+  [0, 3, 6]
+];
 
-//selecting all required elements
-const selectBox = document.querySelector(".select-box"),
-selectBtnX = selectBox.querySelector(".options .playerX"),
-selectBtnO = selectBox.querySelector(".options .playerO"),
-playBoard = document.querySelector(".play-board"),
-players = document.querySelector(".players"),
-allBox = document.querySelectorAll("section span"),
-resultBox = document.querySelector(".result-box"),
-wonText = resultBox.querySelector(".won-text"),
-replayBtn = resultBox.querySelector("button");
+const cells = document.querySelectorAll('.cell');
+document.querySelector('.btn').addEventListener('click', startGame, false);
+startGame();
 
-window.onload = ()=>{ //once window loaded
-    for (let i = 0; i < allBox.length; i++) { //add onclick attribute in all available span
-       allBox[i].setAttribute("onclick", "clickedBox(this)");
+function selectSym(sym){
+  document.querySelector('.play-board').classList.remove("hide");
+  document.querySelector('.play-board').classList.add("show");
+  document.querySelector('.select-box').classList.remove("show");
+  document.querySelector('.select-box').classList.add("hide");
+  huPlayer = sym;
+  aiPlayer = sym==='O' ? 'X' :'O';
+  origBoard = Array.from(Array(9).keys());
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', turnClick, false);
+  }
+  if (aiPlayer === 'X') {
+    
+    turn(bestSpot(),aiPlayer);
+  }
+}
+//document.querySelector(".players").classList.remove("active");
+function startGame() {
+    document.querySelector('.result-box').classList.remove("show");
+    document.querySelector('.result-box').classList.add("hide");
+    document.querySelector('.play-board').classList.remove("show");
+    document.querySelector('.play-board').classList.add("hide");
+    document.querySelector('.select-box').classList.remove("hide");
+    document.querySelector('.select-box').classList.add("show");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+  }
+}
+
+function turnClick(square) {
+  if (typeof origBoard[square.target.id] ==='number') {
+    if(huPlayer == 'X') {
+        document.querySelector(".players").classList.add("active");
+    } else {
+        document.querySelector(".players").classList.remove("active");
     }
-}
-
-selectBtnX.onclick = ()=>{
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
-}
-
-selectBtnO.onclick = ()=>{ 
-    selectBox.classList.add("hide"); //hide select box
-    playBoard.classList.add("show"); //show the playboard section
-    players.setAttribute("class", "players active player"); //set class attribute in players with players active player values
-    bot0();
-}
-
-let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
-let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
-let playerSign = "X"; //this is a global variable beacuse we've used this variable inside multiple functions
-let runBot = true; //this also a global variable with boolen value..we used this variable to stop the bot once match won by someone or drawn
-
-// user click function
-function clickedBox(element){
-    if(players.classList.contains("player")){
-        playerSign = "O"; //if player choose (O) then change playerSign to O
-        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element/box
-        players.classList.remove("active"); ///add active class in players
-        element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-    }else{
-        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element/box
-        players.classList.add("active"); //add active class in players
-        element.setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-    }
-    selectWinner(); //caliing selectWinner function
-    element.style.pointerEvents = "none"; //once user select any box then that box can'be clicked again
-    playBoard.style.pointerEvents = "none"; //add pointerEvents none to playboard so user can't immediately click on any other box until bot select
-    setTimeout(()=>{
-        bot(); //calling bot function
-    },200);
-}
-
-// bot auto select function
-function bot(){
-    let array = []; //creating empty array...we'll store unclicked boxes index
-    if(runBot){ //if runBot is true
-        playerSign = "O"; //change the playerSign to O so if player has chosen X then bot will O
-        for (let i = 0; i < allBox.length; i++) {
-            if(allBox[i].childElementCount == 0){ //if the box/span has no children means <i> tag
-                array.push(i); //inserting unclicked boxes number/index inside array
-            }
+    
+    turn(square.target.id, huPlayer);
+    if (!checkWin(origBoard, huPlayer) && !checkTie())  
+      setTimeout(()=>{
+        turn(bestSpot(), aiPlayer);
+        if(huPlayer == 'X') {
+            document.querySelector(".players").classList.remove("active");
+        } else {
+            document.querySelector(".players").classList.add("active");
         }
-        let randomBox = -1;
-        if(players.classList.contains("player")){ 
-        	randomBox = findIdWin("X");
-        	if(randomBox == -2) {
-        		randomBox = findIdChan("O");
-                if(randomBox == -2) {
-                    randomBox = findIdNuoc2("X",1);
-                    if(randomBox == -2) {
-                        let array = [4, 0, 2, 6, 8, 1, 3, 5, 7]; 
-                        for(let i = 0; i < 9; i++) {
-                            if(allBox[array[i]].childElementCount == 0){ 
-                                randomBox = array[i];
-                                break;
-                            }
-                        }
-                    }
-                }
-        	}
-            playerSign = "X"; //if player has chosen O then bot will X
-            allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot selected element
-            players.classList.add("active"); //remove active class in players
-            allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-        }else{
-        	randomBox = findIdWin("O");
-        	if(randomBox == -2) {
-        		randomBox = findIdChan("X");
-        		if(randomBox == -2) {
-                    randomBox = findIdChanNuoc2("X"); 
-                    if(randomBox == -2) {
-                        randomBox = findIdNuoc2("O",2);
-                        if(randomBox == -2) {
-                            let array = [4, 0, 2, 6, 8, 1, 3, 5, 7]; 
-                            for(let i = 0; i < 9; i++) {
-                                if(allBox[array[i]].childElementCount == 0){ 
-                                    randomBox = array[i];
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-        	}
-            allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot selected element
-            players.classList.remove("active"); //remove active class in players
-            allBox[randomBox].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-        }
-        selectWinner(); //calling selectWinner function
-        allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't click on that box
-        playBoard.style.pointerEvents = "auto"; //add pointerEvents auto in playboard so user can again click on box
-        playerSign = "X"; //if player has chosen X then bot will be O right then we change the playerSign again to X so user will X because above we have changed the playerSign to O for bot
-    }
+      },200);
+  }
 }
 
-function bot0(){
-    let array = [4, 0, 2, 6, 8, 1, 3, 5, 7]; 
-    for(let i = 0; i < 9; i++) {
-    	if(allBox[array[i]].childElementCount == 0){ 
-            playerSign = "X"; //if player has chosen O then bot will X
-			allBox[array[i]].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot selected element
-			players.classList.add("active"); //remove active class in players
-			allBox[array[i]].setAttribute("id", playerSign); //set id attribute in span/box with player choosen sign
-			allBox[array[i]].style.pointerEvents = "none"; //once bot select any box then user can't click on that box
-			playBoard.style.pointerEvents = "auto";
-			return 0;  
-		}     
-    }
+function turn(squareId, player) {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerHTML = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon);
+  checkTie();
 }
 
-function getIdVal(classname){
-    return document.querySelector(".box" + classname).id; //return id value
-}
-function checkIdSign(val1, val2, val3, sign){ //checking all id value is equal to sign (X or O) or not if yes then return true
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return true;
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {index: index, player: player};
+      break;
     }
-}
-function findIdWin(str) {
-    if(checkSign(1, 5, 9, str) != -1)
-		return checkSign(1, 5, 9, str)-1;
-	if(checkSign(3, 5, 7, str) != -1)
-		return checkSign(3, 5, 7, str)-1;
-	if(checkSign(1, 2, 3, str) != -1)
-		return checkSign(1, 2, 3, str)-1;
-	if(checkSign(4, 5, 6, str) != -1)
-		return checkSign(4, 5, 6, str)-1;
-	if(checkSign(7, 8, 9, str) != -1)
-		return checkSign(7, 8, 9, str)-1;
-	if(checkSign(1, 4, 7, str) != -1)
-		return checkSign(1, 4, 7, str)-1;
-	if(checkSign(2, 5, 8, str) != -1)
-		return checkSign(2, 5, 8, str)-1;
-	if(checkSign(3, 6, 9, str) != -1)
-		return checkSign(3, 6, 9, str)-1;
-	return -2;
-}
-function findIdChan(str) {
-    if(checkSign(1, 5, 9, str) != -1)
-		return checkSign(1, 5, 9, str)-1;
-	if(checkSign(3, 5, 7, str) != -1)
-		return checkSign(3, 5, 7, str)-1;
-	if(checkSign(1, 2, 3, str) != -1)
-		return checkSign(1, 2, 3, str)-1;
-	if(checkSign(4, 5, 6, str) != -1)
-		return checkSign(4, 5, 6, str)-1;
-	if(checkSign(7, 8, 9, str) != -1)
-		return checkSign(7, 8, 9, str)-1;
-	if(checkSign(1, 4, 7, str) != -1)
-		return checkSign(1, 4, 7, str)-1;
-	if(checkSign(2, 5, 8, str) != -1)
-		return checkSign(2, 5, 8, str)-1;
-	if(checkSign(3, 6, 9, str) != -1)
-		return checkSign(3, 6, 9, str)-1;
-	return -2;
-}
-function findIdNuoc2(str,so) {
-    if(so == 1) {
-        if(checkSignDi(1, 5, 9, str,so) != -1)
-            return checkSignDi(1, 5, 9, str,so)-1;
-        if(checkSignDi(3, 5, 7, str,so) != -1)
-            return (getIdVal(8) != "") ? 2 : 6;
-    }
-	if(checkSignDi(1, 2, 3, str,so) != -1)
-		return checkSignDi(1, 2, 3, str,so)-1;
-	if(checkSignDi(4, 5, 6, str,so) != -1)
-		return checkSignDi(4, 5, 6, str,so)-1;
-	if(checkSignDi(7, 8, 9, str,so) != -1)
-		return checkSignDi(7, 8, 9, str,so)-1;
-	if(checkSignDi(1, 4, 7, str,so) != -1)
-		return checkSignDi(1, 4, 7, str,so)-1;
-	if(checkSignDi(2, 5, 8, str,so) != -1)
-		return checkSignDi(2, 5, 8, str,so)-1;
-	if(checkSignDi(3, 6, 9, str,so) != -1)
-		return checkSignDi(3, 6, 9, str,so)-1;
-    if(so == 2) {
-        if(checkSignDi(1, 5, 9, str,so) != -1)
-            return checkSignDi(1, 5, 9, str,so)-1;
-        if(checkSignDi(3, 5, 7, str,so) != -1)
-            return checkSignDi(3, 5, 7, str,so)-1;
-    }
-	return -2;
-}
-function findIdChanNuoc2(str) {
-    if(getIdVal(1) == "" && getIdVal(4) == str && getIdVal(7) == "" && getIdVal(8) == "" && getIdVal(9) == str)
-		return 6;
-    if(getIdVal(1) == str && getIdVal(4) == "" && getIdVal(7) == "" && getIdVal(8) == str && getIdVal(9) == "")
-		return 6;
-    if(getIdVal(7) == "" && getIdVal(8) == str && getIdVal(9) == "" && getIdVal(6) == "" && getIdVal(3) == str)
-		return 8;
-    if(getIdVal(7) == str && getIdVal(8) == "" && getIdVal(9) == "" && getIdVal(6) == str && getIdVal(3) == "")
-		return 8;
-    if(getIdVal(9) == str && getIdVal(6) == "" && getIdVal(3) == "" && getIdVal(2) == str && getIdVal(1) == "")
-		return 2;
-    if(getIdVal(9) == "" && getIdVal(6) == str && getIdVal(3) == "" && getIdVal(2) == "" && getIdVal(1) == str)
-		return 2;
-    if(getIdVal(4) == "" && getIdVal(7) == str && getIdVal(3) == "" && getIdVal(2) == str && getIdVal(1) == "")
-		return 0;
-    if(getIdVal(4) == str && getIdVal(7) == "" && getIdVal(3) == str && getIdVal(2) == "" && getIdVal(1) == "")
-		return 0;
-	return -2;
-}
-function checkSign(val1, val2, val3, sign){ //checking all id value is equal to sign (X or O) or not if yes then return true
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == ""){
-        return val3;
-    }
-    if(getIdVal(val1) == sign && getIdVal(val2) == "" && getIdVal(val3) == sign){
-        return val2;
-    }
-    if(getIdVal(val1) == "" && getIdVal(val2) == sign && getIdVal(val3) == sign){
-        return val1;
-    }
-    return -1;
-}
-function checkSignDi(val1, val2, val3, sign,so){ //checking all id value is equal to sign (X or O) or not if yes then return true
-    if(getIdVal(val1) == "" && getIdVal(val2) == sign && getIdVal(val3) == ""){
-        return (Math.floor(Math.random() * so) == 0) ? val3 : val1;
-    }
-    if(getIdVal(val1) == sign && getIdVal(val2) == "" && getIdVal(val3) == ""){
-        return (Math.floor(Math.random() * so) == 0) ? val2 : val3;
-    }
-    if(getIdVal(val1) == "" && getIdVal(val2) == "" && getIdVal(val3) == sign){
-        return (Math.floor(Math.random() * so) == 0) ? val1 : val2;
-    }
-    return -1;
-}
-function selectWinner(){ //if the one of following winning combination match then select the winner
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
-        runBot = false; //passing the false boolen value to runBot so bot won't run again
-        bot(); //calling bot function
-        resultBox.classList.add("show");
-        playBoard.classList.remove("show");
-        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`; //displaying winning text with passing playerSign (X or O)
-    }else{ //if all boxes/element have id value and still no one win then draw the match
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
-            runBot = false; //passing the false boolen value to runBot so bot won't run again
-            bot(); //calling bot function
-            resultBox.classList.add("show");
-            playBoard.classList.remove("show");
-            wonText.textContent = "Match has been drawn!"; //displaying draw match text
-        }
-    }
+  }
+  return gameWon;
 }
 
-replayBtn.onclick = ()=>{
-    window.location.reload(); //reload the current page on replay button click
+function gameOver(gameWon){
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor = 
+      gameWon.player === huPlayer ? "blue" : "red";
+  }
+  for (let i=0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
+  declareWinner(gameWon.player === huPlayer ? "You win!" : "You lose");
+}
+
+function declareWinner(who) {
+    document.querySelector(".won-text").textContent = who;
+    document.querySelector(".result-box").classList.add("show");
+}
+function emptySquares() {
+  return origBoard.filter((elm, i) => i===elm);
+}
+  
+function bestSpot(){
+  return minimax(origBoard, aiPlayer).index;
+}
+  
+function checkTie() {
+  if (emptySquares().length === 0){
+    for (cell of cells) {
+      cell.style.backgroundColor = "green";
+      cell.removeEventListener('click',turnClick, false);
+    }
+    declareWinner("Tie game");
+    return true;
+  } 
+  return false;
+}
+
+function minimax(newBoard, player) {
+  var availSpots = emptySquares(newBoard);
+  
+  if (checkWin(newBoard, huPlayer)) {
+    return {score: -10};
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return {score: 10};
+  } else if (availSpots.length === 0) {
+    return {score: 0};
+  }
+  
+  var moves = [];
+  for (let i = 0; i < availSpots.length; i ++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+    
+    if (player === aiPlayer)
+      move.score = minimax(newBoard, huPlayer).score;
+    else
+       move.score =  minimax(newBoard, aiPlayer).score;
+    newBoard[availSpots[i]] = move.index;
+    if ((player === aiPlayer && move.score === 10) || (player === huPlayer && move.score === -10))
+      return move;
+    else 
+      moves.push(move);
+  }
+  
+  let bestMove, bestScore;
+  if (player === aiPlayer) {
+    bestScore = -1000;
+    for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    bestScore = 1000;
+    for(let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+  
+  return moves[bestMove];
 }
